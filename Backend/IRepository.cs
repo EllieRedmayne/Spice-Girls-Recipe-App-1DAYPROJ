cf055d583522e3ebcab15ac9211df29cf8b9a7e7
Stella Taylor <stellataylor10@gmail.com>
2021-03-05 11:57:01 +0000

    continuing backend

---
+++ Backend/IRepository.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+public interface IRepository<T>
+{
+    Task<IEnumerable<T>> GetAll(); //called on page load
+
+    //Task<IEnumerable<T>> Get(int id);
+
+    // Task<IEnumerable<T>> GetAllByMonth(string month); //on button clicks
+    // Task<T> Insert(T item); //from the form
+}
