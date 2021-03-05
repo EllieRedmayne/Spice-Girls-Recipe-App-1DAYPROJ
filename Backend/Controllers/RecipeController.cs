a2e46c0a9816b9e2b3a4049b82d115e5c252c752
Steve Cadden <s.p.cadden@gmail.com>
2021-03-05 12:18:39 +0000

    controller added

---
+++ Backend/Controllers/RecipeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("Recipes")]
+    public class RecipeController : ControllerBase
+    {
+        private readonly IRepository<Recipe> _recipeRepository;
+
+        public RecipeController(IRepository<Recipe> recipeRepository)
+        {
+            _recipeRepository = recipeRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRecipes()
+        {
+            try {
+                var recipes = await _recipeRepository.GetAll();
+                return Ok(recipes);
+                }
+            catch(Exception){
+                return NotFound($"/recipes/ Not found");
+            }
+        }    
+    }
+}
