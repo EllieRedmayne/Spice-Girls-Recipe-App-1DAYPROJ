50737d0a64dab26a802de70aaf1464c51f9d588e
sara <sara97@live.co.uk>
2021-03-05 11:41:46 +0000

    .

---
+++ Frontend/recipe-app/src/Components/App/App.js
@@ -0,0 +1,23 @@
+//import "./App.css";
+import React, {useState} from "react";
+import Recipe from "../Recipes/index";
+
+function App() {
+  
+  const [id, setId] = useState();
+
+  function handleClick(){
+    let randomRecipe=  Math.floor(Math.random( )* Math.floor(10)) 
+     setId(randomRecipe);
+     console.log(randomRecipe)
+  }
+  
+  return (
+    <div>
+    <button onClick = {(e) => handleClick(e.target.value)}>Get your Recipe</button>
+    <Recipe id = {id}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/recipe-app/src/Components/Recipes/index.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from "react";
+import "../App/App.css";
+
+function Recipe(){
+
+    return(
+        <div>
+            <h1>hi</h1>
+        </div>
+    )
+}
+
+export default Recipe;
